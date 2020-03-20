7b8ac63a0d30203a3a4e4cc00bd24db9dc09b416
Cristian <ccss@utp.edu.co>
2020-03-20 16:34:19 -0500

    showing a tooltip when the words count is less than 150, Note: the words count just check for spaces between letters, that means the user can type a secuens of single letters like 'a a a' and it will work

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -5,11 +5,11 @@ import styled from 'styled-components'
 import styles from './Tooltip_styles'
 
 // Componente base
-const Tooltip_base = ({visible,...props}) => {
+const Tooltip_base = ({visible, closebtn, fz, w, pos, ...props}) => {
     return (
         <div {...props}>
             { props.children }
-            <button className='btn-clean btn-close'>
+            <button className='btn-clean btn-close' onClick={closebtn} >
                 <img src="./src/btn-cerrar.svg" alt="Cerrar tooltip"/>
             </button>
         </div>
--- src/components/Tooltip/Tooltip_styles.js
+++ src/components/Tooltip/Tooltip_styles.js
@@ -2,11 +2,11 @@ import { css } from 'styled-components'
 import Ilex from '../../App/variables'
 const styles = css`
 position: absolute;
-z-index:500;
+z-index:5000;
 top: 0;
 left: 0;
 right:0;
-font-size:0.7em;
+font-size:${props => props.fz || 0.7 }em;
 text-align:center;
 color:${Ilex.textos};
 background-color: #fff;
@@ -14,6 +14,8 @@ padding:2em 1em 2.8em 1em;
 border-radius:1.5em;
 box-shadow:0 0 1em 0 rgba(0,0,0,0.2);
 display:${props => props.visible ? 'block' : 'none'};
+transform: translate(${ props => props.pos || '0'});
+width:${props => props.w + 'em' || '100%' };
 .btn-close{
     position: absolute;
     width:1.5em;
